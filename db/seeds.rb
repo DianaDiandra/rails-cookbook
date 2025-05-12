45adc80bb3a29455226773755c9f91ac5ccb7d3a
Diana Culita <diana.culita100@gmail.com>
2025-05-12 11:49:36 +0100

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Recipe.create!([
+  {
+    name: "Spaghetti Bolognese",
+    description: "Classic Italian pasta dish.",
+    image_url: "https://example.com/images/spaghetti.jpg",
+    rating: 5
+  },
+  {
+    name: "Chickpeas Tikka Masala",
+    description: "Creamy spiced curry.",
+    image_url: "https://example.com/images/tikka.jpg",
+    rating: 4
+  },
+  {
+    name: "Veggie Stir Fry",
+    description: "Mixed vegetables sautéed in a savory soy-ginger sauce.",
+    image_url: "https://example.com/images/stirfry.jpg",
+    rating: 3
+  },
+  {
+    name: "Avocado Toast",
+    description: "Toasted bread topped with mashed avocado and seasoning.",
+    image_url: "https://example.com/images/avocado.jpg",
+    rating: 4
+  }
+])
